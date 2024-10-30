4fdd00ae4f3d8c3cdafa4cf35121decc93ccb5ca
BlazeEMP <blazelitanui@gmail.com>
2024-10-29 19:43:16 -0500

    refined input javascript functions, read and write to local storage complete, still need JS to build HTML divs and append to body of index.html

---
+++ assets/JS/script.js
@@ -0,0 +1,41 @@
+let loadedReview = {};
+const main = document.body.main;
+
+function renderPage() {
+    let allReviews = readLocalStorage();
+    for (review of allReviews) {
+        username = review.username;
+        title = review.title;
+        rating = review.rating;
+        content = review.content;
+        console.log(review);
+    }
+    console.log(allReviews);
+}
+
+function readLocalStorage() {
+    let array = [];
+    const storedPosts = JSON.parse(localStorage.getItem('reviewData'));
+    // console.log(storedPosts);
+    if (storedPosts == null) {
+        return array;
+        // console.log(array);
+    }
+    else {
+        array = storedPosts;
+        return array;
+    }
+}
+
+// object used for loading new data and pushing into review lists, if order of files changes may want to include in input.js
+let tempStorageObject = {};
+
+function storeLocalStorage(newObject) {
+    tempStorageObject = newObject;
+    let fetchedReviews = readLocalStorage();
+    fetchedReviews.push(newObject);
+    console.log(fetchedReviews);
+    localStorage.setItem('reviewData', JSON.stringify(fetchedReviews));
+}
+
+renderPage();
